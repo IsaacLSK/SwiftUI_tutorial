7716cfa074b72294426e413c548bdb679f90d3a4
Issac Lai <isklai@fehd.gov.hk>
2024-12-30 12:09:43 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by RM-ITS-046 on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
